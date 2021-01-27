8e168c0e3959bd6e82c2e4b281106cc652f0ea08
botto54 <bryanlbottorff@gmail.com>
2021-01-26 22:34:22 -0500

    add thunk, react router doom, remove componentdidmount fetch from app.js used to test api connection, ps data is succesfully fetched from rails api

--- src/App.js
+++ src/App.js
@@ -1,29 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome Bryan, let's begin your final project, a new beginning is in sight!
-        </p><br></br>
-        <p>Don't forget to tell your wifey you love her!</p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  // componentDidMount(){
+  //   fetch('http://localhost:3000/accounts/1/transactions')
+  //   .then(resp => resp.json())
+  //   .then(data => console.log(data))
+  // }
+
+  render(){
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome Bryan, let's begin your final project, a new beginning is in sight!
+          </p><br></br>
+          <p>Don't forget to tell your wifey you love her!</p>
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
